8ef8186e0cd0883d33f77a5af497f44c81f65571
jaebradley <jae.b.bradley@gmail.com>
2017-02-12 03:04:56 -0500

    add missing parameter

--- nba_statistics/settings.py
+++ nba_statistics/settings.py
@@ -14,7 +14,7 @@ import os
 
 import dj_database_url
 
-from environment import SECRET_KEY
+from environment import SECRET_KEY, DATABASE_URL
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
