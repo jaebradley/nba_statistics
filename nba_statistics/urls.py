4a9d2e41130899299e712d29c56d131da93a7abe
jaebradley <jae.b.bradley@gmail.com>
2017-02-12 12:51:40 -0500

    build urls

--- nba_statistics/urls.py
+++ nba_statistics/urls.py
@@ -1,4 +1,4 @@
-"""nba_statistics URL Configuration
+"""dfs_site_data URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
@@ -14,9 +14,34 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
-from django.contrib import admin
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+
+from data.views import team_detail, teams_list, players_list, games_list, player_detail, game_detail, seasons_list, \
+    season_detail, positions_list, position_detail, game_player_box_scores_list, game_player_box_score_detail
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+
+    url(r'^teams/$', teams_list, name='teams_list'),
+    url(r'^teams/(?P<team_id>[0-9]+)/$', team_detail, name='team_detail'),
+
+    url(r'^players/$', players_list, name='players_list'),
+    url(r'^players/(?P<player_id>[0-9]+)$',
+        player_detail, name='player_detail'),
+
+    url(r'^games/$', games_list, name='games_list'),
+    url(r'^games/(?P<game_id>[0-9]+)/$', game_detail, name='game_detail'),
+
+    url(r'^seasons/$', seasons_list, name='seasons_list'),
+    url(r'^seasons/(?P<season_id>[0-9]+)/$', season_detail, name='season_detail'),
+
+    url(r'^positions/$', positions_list, name='positions_list'),
+    url(r'^positions/(?P<position_id>[0-9]+)/$', position_detail, name='position_detail'),
+
+    url(r'^box-scores/$', game_player_box_scores_list, name='game_player_box_scores_list'),
+    url(r'^box-scores/(?P<box_score_id>[0-9]+)/$', game_player_box_score_detail, name='game_player_box_score_detail')
 ]
